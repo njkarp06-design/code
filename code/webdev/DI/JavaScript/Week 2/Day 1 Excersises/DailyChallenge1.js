6c4b27e6e8de4c084a3cc64fa4765341089b3fab
njkarp06-design <njkarp06@gmail.com>
2026-03-30 14:58:08 +0300

    commit

--- code/webdev/DI/JavaScript/Week 2/Day 1 Excersises/DailyChallenge1.js
+++
@@ -1,8 +0,0 @@
-let sentence = "The movie is not that bad, I like it."
-x = console.log(sentence.indexOf("not"))
-y = console.log(sentence.indexOf("bad"))
-if (x < y) {
-console.log("The movie is good, I like it.")
-} else {
-    console.log(sentence)
-}
